edc34a3cbe8882777c61b823b3ea21026f57e925
Your Name <youremail@gmail.com>
2026-02-03 14:42:05 +0000

    updated......

---
+++ test.py
@@ -0,0 +1,4 @@
+i love python
+i know lite bit of coding
+i hate overthinking
+i am a very good person
---
+++ test2.sh
@@ -0,0 +1,7 @@
+i am shell scripting program
+i am veryy good at coding
+hii
+ how are you 
+i loveeee uuu
+i hate u
+
